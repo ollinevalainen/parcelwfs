fb8dbf16130af22786dfb9c7ec48836ef42ae0b1
Olli Nevalainen <olli.nevalainen.fmi@gmail.com>
2023-12-05 18:16:02 +0200

    Species info with parcel ID

--- ruokavirasto_wfs/ruokavirasto_wfs.py
+++ ruokavirasto_wfs/ruokavirasto_wfs.py
@@ -17,13 +17,17 @@ FIELD_PARCEL_ID_PROPERTY = "PERUSLOHKOTUNNUS"
 FIELD_PARCEL_SPECIES_ID_FI = "KASVIKOODI"
 FIELD_PARCEL_SPECIES_DESCRIPTION_FI = "KASVIKOODI_SELITE_FI"
 
+
 def get_available_layers() -> list:
     wfs = WebFeatureService(url=FINNISH_FOOD_AUTHORITY_WFS, version="2.0.0")
     return list(wfs.contents)
 
+
 def get_available_parcel_layers() -> list:
     all_layers = get_available_layers()
-    field_parcel_layers = [layer for layer in all_layers if FIELD_PARCEL_LAYER_BASENAME in layer]
+    field_parcel_layers = [
+        layer for layer in all_layers if FIELD_PARCEL_LAYER_BASENAME in layer
+    ]
     return field_parcel_layers
 
 
@@ -33,19 +37,28 @@ def get_available_parcel_years() -> list:
     return years_available
 
 
-def _get_parcel(query_filter:str, year: int) -> gpd.GeoDataFrame:
-
+def _get_parcel(query_filter: str, year: int) -> gpd.GeoDataFrame:
     years_available = get_available_parcel_years()
     if year not in years_available:
-        raise ValueError(f"""Field parcel layer not available for year {year}. Currently
-                         available years: {years_available}""")
-    
+        raise ValueError(
+            f"""Field parcel layer not available for year {year}. Currently
+                         available years: {years_available}"""
+        )
+
     layer_name = f"{FIELD_PARCEL_LAYER_BASENAME}{year}"
-    params = dict(service='WFS', version="2.0.0", request='GetFeature',
-        typeName=layer_name, cql_filter=query_filter, outputFormat='json')
+    params = dict(
+        service="WFS",
+        version="2.0.0",
+        request="GetFeature",
+        typeName=layer_name,
+        cql_filter=query_filter,
+        outputFormat="json",
+    )
 
     # Parse the URL with parameters
-    wfs_request_url = requests.Request("GET",FINNISH_FOOD_AUTHORITY_WFS, params=params).prepare().url
+    wfs_request_url = (
+        requests.Request("GET", FINNISH_FOOD_AUTHORITY_WFS, params=params).prepare().url
+    )
 
     # Read data from URL
     try:
@@ -68,7 +81,7 @@ def get_parcel_by_parcel_id(parcel_id: str, year: int) -> gpd.GeoDataFrame:
 def get_parcel_by_point3067(point_in_epsg3067: Point, year: int) -> gpd.GeoDataFrame:
     x = point_in_epsg3067.x
     y = point_in_epsg3067.y
-    spatial_filter=f"Intersects(geom,POINT ({x} {y}))"
+    spatial_filter = f"Intersects(geom,POINT ({x} {y}))"
     # Read data from URL
     gdf = _get_parcel(spatial_filter, year)
     return gdf
@@ -90,8 +103,7 @@ def get_parcels_by_parcel_id(parcel_ids: List[str], year: int) -> gpd.GeoDataFra
     return gdf
 
 
-def get_parcel_species(lat:float , lon:float, year: Optional[int]=None) -> dict:
-
+def get_parcel_species(lat: float, lon: float, year: Optional[int] = None) -> dict:
     if year:
         years = [year]
     else:
@@ -101,8 +113,27 @@ def get_parcel_species(lat:float , lon:float, year: Optional[int]=None) -> dict:
     for year in years:
         gdf = get_parcel_by_lat_lon(lat, lon, year)
         species_information[year] = {
-                                    "parcel_id": gdf[FIELD_PARCEL_ID_PROPERTY][0],
-                                    "species_code_FI": gdf[FIELD_PARCEL_SPECIES_ID_FI][0],
-                                    "species_description_FI": gdf[FIELD_PARCEL_SPECIES_DESCRIPTION_FI][0]}
+            "parcel_id": gdf[FIELD_PARCEL_ID_PROPERTY][0],
+            "species_code_FI": gdf[FIELD_PARCEL_SPECIES_ID_FI][0],
+            "species_description_FI": gdf[FIELD_PARCEL_SPECIES_DESCRIPTION_FI][0],
+        }
+
+    return species_information
+
+
+def get_parcel_species_by_parcel_id(parcel_id: str, year: Optional[int] = None) -> dict:
+    if year:
+        years = [year]
+    else:
+        years = get_available_parcel_years()
+
+    species_information = {}
+    for year in years:
+        gdf = get_parcel_by_parcel_id(parcel_id, year)
+        species_information[year] = {
+            "parcel_id": gdf[FIELD_PARCEL_ID_PROPERTY][0],
+            "species_code_FI": gdf[FIELD_PARCEL_SPECIES_ID_FI][0],
+            "species_description_FI": gdf[FIELD_PARCEL_SPECIES_DESCRIPTION_FI][0],
+        }
 
     return species_information
